4e14ed6ddee6452ff16131229c820d1c33ee2ca9
Julian McGinnis <julian.mcginnis@park-depot.de>
2022-06-07 13:09:07 +0200

    clarify output and add networkx example

--- source/pytorch_dataset/test_link_graph.py
+++ source/pytorch_dataset/test_link_graph.py
@@ -7,6 +7,8 @@ import pandas as pd
 import argparse
 import os
 import torch_geometric.transforms as T
+from torch_geometric.utils import to_networkx
+from torch_geometric.data import Data
 
 parser = argparse.ArgumentParser(description='display graph features and summary.')
 parser.add_argument('-ds','--dataset',help='Specify the dataset you want to select', required=True)
@@ -33,23 +35,34 @@ def main():
     print('==============================================================')
 
     # Gather some statistics about the graph.
-    print(f'Number of nodes: {data.num_nodes}')
-    print(f'Number of edges: {data.num_edges}')
+    print(f'Number of nodes in graph: {data.num_nodes}')
+    print(f'Number of edges in graph: {data.num_edges}')
     print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
     print(f'Contains isolated nodes: {data.contains_isolated_nodes()}')
     print(f'Contains self-loops: {data.contains_self_loops()}')
     print(f'Is Undirected: {data.is_undirected()}')
 
-    print(data.edge_index_undirected)
-    print(data.edge_index)
+    print(f'Number of undirected edges', data.edge_index_undirected.size(dim=1))
+    print(f'Number of training edges', data.train_pos_edge_index.size(dim=1))
+    print(f'Number of validation edges', data.val_pos_edge_index.size(dim=1))
+    print(f'Number of test edges', data.test_pos_edge_index.size(dim=1))
 
-    print(data.train_pos_edge_index.shape)
-    print(data.test_pos_edge_index.shape)
-    print(data.val_pos_edge_index.shape)
+    # Caution: if you would like to convert all edges to networkx graph, please
+    # overwrite data.edge_index with data.edge_index_undirected. 
+    # The link dataset adheres to the convention that only training edges are 
+    # present in the data.edge_index. However, to obtain the full graph, we have to pass
+    # all edges to the networkx function.
 
-    print(data.train_neg_edge_index.shape)
-    print(data.test_neg_edge_index.shape)
-    print(data.val_neg_edge_index.shape)
+    data_undirected = Data(x=data.x, edge_index = data.edge_index_undirected,
+                      edge_attr = data.edge_attr_undirected)
+
+    G = to_networkx (data_undirected, to_undirected=False)
+    print("Networkx: #nodes, #edges", G.number_of_nodes(), G.number_of_edges())
+
+
+
+
+
 
 
 if __name__ == "__main__":
